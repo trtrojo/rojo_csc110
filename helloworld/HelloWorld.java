9d2be6219c7c38b1b4869bc2a973e82d26da1872
rew0rk <rew0rk@dodgeJunction.local.n-systems.us>
2016-09-24 16:36:27 -0400

    Created gitignore file, ignorign .class files

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld
 /* There is the class header above */
  public static void main(String[] args)
   {
-   System.out.print("Hello, world");
+   System.out.println("Hello, world");
    System.out.println("Hello, java");
+   System.out.print("Next stop is Sumtra");
+   System.out.println(" or Sualevery");
+   //System.out.print("testing newline character...\n\n");
   }
 }
