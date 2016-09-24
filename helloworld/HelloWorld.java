4fae86f6d93375c755304f3b34bd1b6746615e0e
rew0rk <rew0rk@dodgeJunction.local.n-systems.us>
2016-09-24 16:29:47 -0400

    added to HelloWorld app

--- helloworld/HelloWorld.java
+++ helloworld/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld
 /* There is the class header above */
  public static void main(String[] args)
   {
-   System.out.println("Hello, world");
+   System.out.print("Hello, world");
+   System.out.println("Hello, java");
   }
 }
