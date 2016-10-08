0a8e8df83bc982f959fc75f1ff9570d518b14f5c
rew0rk <rew0rk@dodgeJunction.local.n-systems.us>
2016-10-08 13:58:14 -0400

    Math Demo (Lesson 4)

---
+++ lesson4_mathdemo/MathDemo.java
@@ -0,0 +1,11 @@
+public class MathDemo {
+ public static void main(String[] args) {
+  double monday, tuesday, wednesday, average;
+  
+  monday = 100.0;
+  tuesday = 200.0;
+  wednesday = 300.0;
+  average = (monday + tuesday + wednesday) / 3;
+  System.out.println("The average is " + average);
+ }
+}
